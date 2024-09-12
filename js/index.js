0d92ef96f80d09d328c6794e8a15c006b6bbc31a
Sunyoung Lee <s3u0n4@gmail.com>
2024-09-13 01:40:12 +0900

    wordle last
    wordle

--- js/index.js
+++ js/index.js
@@ -2,18 +2,29 @@ const answer = "APPLE";
 
 let attempts = 0;
 let index = 0;
+let timer;
 
 function appStart() {
-  const nextLine = () => {
-    if (attempts === 6) return;
-    gameover();
-    attempts += 1;
-    index = 0;
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료됐습니다.";
+    div.style =
+      "display:flex; justify-content:center; align-items : center; position:absolute; top:40vh; left:39.5vw; background-color:white; width:200px;height:100px";
+    document.body.appendChild(div);
   };
 
   const gameover = () => {
     window.removeEventListener("keydown", handleKeydown);
+    displayGameover();
+    clearInterval(timer);
   };
+
+  const nextLine = () => {
+    if (attempts === 6) return gameover();
+    attempts += 1;
+    index = 0;
+  };
+
   const handleEnterKey = () => {
     let correct = 0;
     for (let i = 0; i < 5; i++) {
@@ -22,14 +33,27 @@ function appStart() {
       );
       const input_word = block.innerText;
       const asnwer_word = answer[i];
-      if (input_word === asnwer_word) block.style.background = "#6AAA64";
-      else if (answer.includes(input_word)) block.style.background = "#C9B458";
+      if (input_word === asnwer_word) {
+        correct += 1;
+        block.style.background = "#6AAA64";
+      } else if (answer.includes(input_word))
+        block.style.background = "#C9B458";
       else block.style.background = "#787C7E";
       block.style.color = "white";
     }
 
     if (correct === 5) gameover();
     else nextLine();
+  };
+
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-column[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index !== 0) index -= 1;
   };
 
   const handleKeydown = (event) => {
@@ -39,7 +63,8 @@ function appStart() {
       `.board-column[data-index='${attempts}${index}']`
     );
 
-    if (index === 5) {
+    if (event.key === "Backspace") handleBackspace();
+    else if (index === 5) {
       if (event.key === "Enter") handleEnterKey();
       else return;
     } else if (65 <= keyCode && keyCode <= 90) {
@@ -47,6 +72,22 @@ function appStart() {
       index += 1;
     }
   };
+
+  const startTimer = () => {
+    const 시작_시간 = new Date();
+
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+      const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${분}:${초}`;
+    }
+    timer = setInterval(setTime, 1000);
+  };
+
+  startTimer();
 
   window.addEventListener("keydown", handleKeydown);
 }
